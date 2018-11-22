dc0e44b287ef8f97ee231e7868c1caaf2e88cd35
Yang Ming <yangming2004@gmail.com>
2018-11-22 21:57:01 +0800

    add support write report to phabricator phame(blog)

--- phawrsre.php
+++ phawrsre.php
@@ -15,14 +15,6 @@ $user = $client->callMethodSynchronous('user.query', $api_parameters_user);
 $user_phid = $user[0]['phid'];
 $user_realName = $user[0]['realName'];
 
-$api_parameters = [
-    'ownerPHIDs'    => [$user_phid],
-    'order'         => 'order-modified',
-];
-
-$client = (new ConduitClient($config['url']))->setConduitToken($config['api_token']);
-$tasks = $client->callMethodSynchronous('maniphest.query', $api_parameters);
-
 $offset = $config['weekday_start'];
 $days = $config['days'];
 $week_offset = $config['week_before'];
@@ -32,20 +24,22 @@ $week = $currentDate->format("W") - $week_offset;
 
 $start = (new DateTime())->setISODate($year, $week, $offset)->setTime(0, 0, 0);
 $end = (new DateTime())->setISODate($year, $week, $offset + 6)->setTime(23, 59, 59);
+$start_int = $start->getTimestamp();
+$end_int = $end->getTimestamp();
 
 $subject = sprintf(
-    'Workly report of Week %s, %s (%s ~ %s) for %s',
-    $week,
+    '%s的工作周报（%s 年第 %s 周，%s ~ %s）',
+    $user_realName,
     $year,
+    $week,
     $start->format('m.d'),
-    $end->format('m.d'),
-    $user_realName
+    $end->format('m.d')
 );
 
 $email_subject = sprintf(
-    'Week %s, %s (%s ~ %s)',
-    $week,
+    '%s Week %s(%s ~ %s)',
     $year,
+    $week,
     $start->format('m.d'),
     $end->format('m.d')
 );
@@ -57,31 +51,60 @@ $output = [
     'other'       => []
 ];
 
-foreach ($tasks as $key => $task)
+$api_parameters_task_closed = [
+    'constraints'   =>  [
+        "assigned"  =>  [$user_phid],
+        "statuses"  =>  [
+            "resolved"
+        ],
+        "closedStart"   =>  $start_int,
+        "closedEnd" =>  $end_int
+    ]
+];
+$tasks = $client->callMethodSynchronous('maniphest.search', $api_parameters_task_closed);
+foreach ($tasks['data'] as $key => $task)
 {
     $dateModified = new DateTime(date("Y-m-d H:i:s", $task['dateModified']));
 
-    if ($dateModified > $start and $dateModified < $end)
-    {
-        $outputString = sprintf(
-            "# %s(T%s)\n",
-            $task['title'],
-            $task['id']
-        );
+    $outputString = sprintf(
+        "# %s(T%s)\n",
+        $task['fields']['name'],
+        $task['id']
+    );
 
-        switch ($task['status'])
-        {
-            case "open":
-                $output['ongoing'][] = $outputString;
-                break;
+    $output['completed'][] = $outputString;
+}
 
-            case "resolved":
-                $output['completed'][] = $outputString;
-                break;
+$api_parameters_task_open = [
+    'constraints'   =>  [
+        "assigned"  =>  [$user_phid],
+        "statuses"  =>  ["open"],
+        "modifiedStart" =>  $start_int,
+        "createdEnd"    =>  $end_int
+    ]
+];
 
-            default:
-                $output['other'][] = $outputString.$task['status'];
-                break;
+$tasks = $client->callMethodSynchronous('maniphest.search', $api_parameters_task_open);
+$taskopen = array();
+foreach ($tasks['data'] as $key => $task)
+{
+    $taskopen[$task['id']] = $task['fields']['name'];
+}
+
+$api_parameters_tran = [
+    'ids'   => array_keys($taskopen),
+];
+$trans = $client->callMethodSynchronous('maniphest.gettasktransactions', $api_parameters_tran);
+
+foreach ($taskopen as $task_id => $task_title) {
+    foreach ($trans[$task_id] as $key => $tran) {
+        if ($tran['dateCreated'] > $start_int and $tran['dateCreated'] < $end_int) {
+            $output['ongoing'][] = sprintf(
+                "# %s(T%s)\n",
+                $task_title,
+                $task_id
+            );
+            break;
         }
     }
 }
@@ -95,22 +118,47 @@ $message = sprintf(
 
 print $message;
 
-$email_subject = "=?UTF-8?B?". base64_encode("$email_subject"). "?=";
+$email_subject_encode = "=?UTF-8?B?". base64_encode("$email_subject"). "?=";
 $from_email = $config['email'];
 $headers = "From: $from_email\r\n". 
         "MIME-Version: 1.0". "\r\n". 
         "Content-type: text/plain; charset=UTF-8". "\r\n";
 
 if ($config['send2on']) {
-    $re = mail($config['email4on'], $email_subject, $message, $headers, " -f $from_email");
+    $re = mail($config['email4on'], $email_subject_encode, $message, $headers, " -f $from_email");
     if (!$re) {
         echo error_get_last()['message'];
     }
 }
 
 if ($config['send2en']) {
-    $re = mail($config['email4en'], $email_subject, $message, $headers, " -f $from_email");
+    $re = mail($config['email4en'], $email_subject_encode, $message, $headers, " -f $from_email");
     if (!$re) {
         echo error_get_last()['message'];
     }
+}
+
+if ($config['write2phame']) {
+    $blog_phid = $config['phid_blog'];
+    $api_parameters_blog = [
+        'transactions'  => [
+            [
+                "type"  => "blog",
+                "value" => "$blog_phid"
+            ],
+            [
+                "type"  =>  "title",
+                "value" =>  "$subject"
+            ],
+            [
+                "type"  =>  "subtitle",
+                "value" =>  "$email_subject"
+            ],
+            [
+                "type"  =>  "body",
+                "value" =>  "$message"
+            ],
+        ],
+    ];
+    $result = $client->callMethodSynchronous('phame.post.edit', $api_parameters_blog);
 }
